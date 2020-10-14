f1d72d093a1b160e2bdbb4bef0b0dc998e898a05
Kamil Jopek <kamil.jopek@leadfeeder.com>
2020-10-14 15:30:28 -0500

    Updates

---
+++ precache-manifest.0e67203e8ceb848d06d54782021ed6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9453761359d580e60478e071ab1397a0",
+    "url": "/ember-typescript-interface-generator/index.html"
+  },
+  {
+    "revision": "50df20e2263bac131d48",
+    "url": "/ember-typescript-interface-generator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7ebf90aff1e003d593e7",
+    "url": "/ember-typescript-interface-generator/static/js/2.824581fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ember-typescript-interface-generator/static/js/2.824581fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50df20e2263bac131d48",
+    "url": "/ember-typescript-interface-generator/static/js/main.1d0524f6.chunk.js"
+  },
+  {
+    "revision": "a443b1e27c38309156e0",
+    "url": "/ember-typescript-interface-generator/static/js/runtime-main.d629e19a.js"
+  }
+]);
